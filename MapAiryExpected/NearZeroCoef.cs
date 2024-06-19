a4c026957c98efcf708afe919cab3c56fd7070c4
tk-yoshimura <yoshimura8192@gmail.com>
2024-06-19 20:09:59 +0900

    add: nearzero coef

---
+++ MapAiryExpected/NearZeroCoef.cs
@@ -0,0 +1,106 @@
+﻿namespace MapAiryExpected {
+    public static class NearZeroCoef {
+        private static readonly Dictionary<long, Fraction> pdf_terms = [];
+        private static readonly Dictionary<long, Fraction> cdf_terms = [];
+
+        private static readonly Dictionary<long, Fraction> pdf_exp_terms = new(){
+            { 0, 1 }, { 1, -1 }, { 2, 0 }, { 3, 1 }, { 4, -new Fraction(1, 2) }, { 5, 0 }
+        };
+        private static readonly Dictionary<long, Fraction> cdf_exp_terms = [];
+
+        public static Fraction PDFExpTerm(long i) {
+            ArgumentOutOfRangeException.ThrowIfNegative(i);
+
+            if (pdf_exp_terms.TryGetValue(i, out Fraction? value)) {
+                return value;
+            }
+
+            if ((i % 3) == 2) {
+                pdf_exp_terms.Add(i, 0);
+            }
+            else {
+                long n = i / 6, k = i % 6;
+
+                Fraction f = PDFExpTerm(i - 6);
+                if (k == 0) {
+                    f /= checked((3 * n - 2) * (3 * n));
+                }
+                else if (k == 1) {
+                    f /= checked((3 * n - 1) * (3 * n));
+                }
+                else if (k == 3) {
+                    f /= checked((3 * n) * (3 * n + 1));
+                }
+                else {
+                    f /= checked((3 * n) * (3 * n + 2));
+                }
+
+                pdf_exp_terms.Add(i, f);
+            }
+
+            return pdf_exp_terms[i];
+        }
+
+        public static Fraction PDFTerm(long i) {
+            ArgumentOutOfRangeException.ThrowIfNegative(i);
+
+            if (pdf_terms.TryGetValue(i, out Fraction? value)) {
+                return value;
+            }
+
+            Fraction f = 0;
+            for (long j = i % 3; j <= i; j += 3) {
+                Fraction g = PDFExpTerm(j);
+
+                for (long k = 1; k <= (i - j) / 3; k++) {
+                    g *= new Fraction(2, 3) / k;
+                }
+
+                f += g;
+            }
+
+
+            pdf_terms.Add(i, f);
+
+            return pdf_terms[i];
+        }
+
+        public static Fraction CDFTerm(long i) {
+            ArgumentOutOfRangeException.ThrowIfNegative(i);
+
+            if (cdf_terms.TryGetValue(i, out Fraction? value)) {
+                return value;
+            }
+
+            Fraction f = PDFTerm(i);
+
+            cdf_terms.Add(i, f / (i + 1));
+
+            return cdf_terms[i];
+        }
+
+        public static Fraction CDFExpTerm(long i) {
+            ArgumentOutOfRangeException.ThrowIfNegative(i);
+
+            if (cdf_exp_terms.TryGetValue(i, out Fraction? value)) {
+                return value;
+            }
+
+            Fraction f = 0;
+            for (long j = i % 3; j <= i; j += 3) {
+                Fraction g = CDFTerm(j);
+
+                for (long k = 1; k <= (i - j) / 3; k++) {
+                    g *= new Fraction(-2, 3) / k;
+                }
+
+                f += g;
+            }
+
+
+            cdf_exp_terms.Add(i, f);
+
+            return cdf_exp_terms[i];
+        }
+    }
+}
---
+++ MapAiryExpectedTest/NearZeroCoefTest.cs
@@ -0,0 +1,72 @@
+using MapAiryExpected;
+using MultiPrecision;
+using System.Numerics;
+
+namespace MapAiryExpectedTest {
+    [TestClass]
+    public class NearZeroCoefTest {
+        [TestMethod]
+        public void PDFExpCoefTest() {
+            Assert.AreEqual(new(1, 268334780313600), NearZeroCoef.PDFExpTerm(45));
+            Assert.AreEqual(new(-1, 1061932177152000), NearZeroCoef.PDFExpTerm(46));
+            Assert.AreEqual(0, NearZeroCoef.PDFExpTerm(47));
+            Assert.AreEqual(new(1, 6440034727526400), NearZeroCoef.PDFExpTerm(48));
+            Assert.AreEqual(new(-1, 25486372251648000), NearZeroCoef.PDFExpTerm(49));
+            Assert.AreEqual(0, NearZeroCoef.PDFExpTerm(50));
+
+            for (int i = 0; i < 60; i++) {
+                Fraction f = NearZeroCoef.PDFExpTerm(i);
+
+                Console.WriteLine($"{i}\t{f}");
+            }
+        }
+
+        [TestMethod]
+        public void PDFCoefTest() {
+            Assert.AreEqual(new(77081860213, BigInteger.Parse("713020519923199488000")), NearZeroCoef.PDFTerm(45));
+            Assert.AreEqual(new(-23569729, 677496156251904000), NearZeroCoef.PDFTerm(46));
+            Assert.AreEqual(0, NearZeroCoef.PDFTerm(47));
+            Assert.AreEqual(new(1640039579, BigInteger.Parse("180131499770071449600")), NearZeroCoef.PDFTerm(48));
+            Assert.AreEqual(new(-2286263713, BigInteger.Parse("796735479752239104000")), NearZeroCoef.PDFTerm(49));
+            Assert.AreEqual(0, NearZeroCoef.PDFExpTerm(50));
+
+            for (int i = 0; i < 60; i++) {
+                Fraction f = NearZeroCoef.PDFTerm(i);
+
+                Console.WriteLine($"{i}\t{f}");
+            }
+        }
+
+        [TestMethod]
+        public void CDFExpCoefTest() {
+            Assert.AreEqual(new(-3383087, BigInteger.Parse("9240544683286076160000")), NearZeroCoef.CDFExpTerm(45));
+            Assert.AreEqual(new(5547827, BigInteger.Parse("64495011224067372748800")), NearZeroCoef.CDFExpTerm(46));
+            Assert.AreEqual(0, NearZeroCoef.CDFExpTerm(47));
+            Assert.AreEqual(new(2755547239, BigInteger.Parse("152136327665621957898240000")), NearZeroCoef.CDFExpTerm(48));
+            Assert.AreEqual(new(-116796193, BigInteger.Parse("27640719096028874035200000")), NearZeroCoef.CDFExpTerm(49));
+            Assert.AreEqual(0, NearZeroCoef.CDFExpTerm(50));
+
+            for (int i = 0; i < 60; i++) {
+                Fraction f = NearZeroCoef.CDFExpTerm(i);
+
+                Console.WriteLine($"{i}\t{f}");
+            }
+        }
+
+        [TestMethod]
+        public void CDFCoefTest() {
+            Assert.AreEqual(new(77081860213, BigInteger.Parse("32798943916467176448000")), NearZeroCoef.CDFTerm(45));
+            Assert.AreEqual(new(-23569729, BigInteger.Parse("31842319343839488000")), NearZeroCoef.CDFTerm(46));
+            Assert.AreEqual(0, NearZeroCoef.CDFTerm(47));
+            Assert.AreEqual(new(1640039579, BigInteger.Parse("8826443488733501030400")), NearZeroCoef.CDFTerm(48));
+            Assert.AreEqual(new(-2286263713, BigInteger.Parse("39836773987611955200000")), NearZeroCoef.CDFTerm(49));
+            Assert.AreEqual(0, NearZeroCoef.CDFTerm(50));
+
+            for (int i = 0; i < 60; i++) {
+                Fraction f = NearZeroCoef.CDFTerm(i);
+
+                Console.WriteLine($"{i}\t{f}");
+            }
+        }
+    }
+}
